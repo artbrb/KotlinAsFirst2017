f6edf206010de3901149780f1105875821d728fe
Artem B <artem.barabolenko@gmail.com>
2017-09-16 22:27:32 +0300

    lesson 2 task 1

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -123,6 +123,7 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
 fun accountInThreeYears(initial: Int, percent: Int): Double {
     val x1 = initial * (1.0 + percent/100.0) * (1.0 + percent/100.0) * (1.0 + percent/100.0)
     return x1
+
 }
 
 
