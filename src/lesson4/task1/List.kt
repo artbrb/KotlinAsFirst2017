6b4e74fb40758bc5fde7728c936acd838c69f7d6
Artem B <artem.barabolenko@gmail.com>
2017-10-23 22:17:09 +0300

    fix lesson 3 , lesson 4

--- src/lesson4/task1/List.kt
+++ src/lesson4/task1/List.kt
@@ -2,6 +2,8 @@
 package lesson4.task1
 
 import lesson1.task1.discriminant
+import java.lang.Math.pow
+import java.lang.Math.sqrt
 
 /**
  * Пример
@@ -106,14 +108,24 @@ fun buildSumExample(list: List<Int>) = list.joinToString(separator = " + ", post
  * по формуле abs = sqrt(a1^2 + a2^2 + ... + aN^2).
  * Модуль пустого вектора считать равным 0.0.
  */
-fun abs(v: List<Double>): Double = TODO()
+fun abs(v: List<Double>): Double {
+    if (v.isEmpty()) return 0.0
+    var sum = 0.0
+    for (element in v) {
+        sum += pow(element, 2.0)
+    }
+    return sqrt(sum)
+}
 
 /**
  * Простая
  *
  * Рассчитать среднее арифметическое элементов списка list. Вернуть 0.0, если список пуст
  */
-fun mean(list: List<Double>): Double = TODO()
+fun mean(list: List<Double>): Double {
+    if (list.isEmpty()) return 0.0
+    return list.sum() / list.size
+}
 
 /**
  * Средняя
@@ -123,7 +135,15 @@ fun mean(list: List<Double>): Double = TODO()
  *
  * Обратите внимание, что данная функция должна изменять содержание списка list, а не его копии.
  */
-fun center(list: MutableList<Double>): MutableList<Double> = TODO()
+fun center(list: MutableList<Double>): MutableList<Double> {
+    val middleSum = list.sum() / list.size
+    if (list.isNotEmpty()) {
+        for (i in 0 until list.size) {
+            list[i] -= middleSum
+        }
+    }
+    return list
+}
 
 /**
  * Средняя
@@ -132,7 +152,15 @@ fun center(list: MutableList<Double>): MutableList<Double> = TODO()
  * представленные в виде списков a и b. Скалярное произведение считать по формуле:
  * C = a1b1 + a2b2 + ... + aNbN. Произведение пустых векторов считать равным 0.0.
  */
-fun times(a: List<Double>, b: List<Double>): Double = TODO()
+fun times(a: List<Double>, b: List<Double>): Double {
+    var c = 0.0
+    if (a.isNotEmpty()) {
+        for (i in 0 until a.size) {
+            c += a[i] *  b[i]
+        }
+    }
+    return c
+}
 
 /**
  * Средняя
